2e1aa958e89b2aa83079b7e9e07026c3797947cd
mnelisi90 <nmnelysn@gmail.com>
2024-12-16 13:35:22 +0200

    Calculate simple interest for a period of 3 years

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# define variables
+principal = 1000   # value representing $1000
+rate = 0.05        # represents 5% annual interest
+time = 3           # period of 3 years
+
+# calculate simple interest rate
+interest = principal * rate * time
+
+# print results in a readable format
+print("The simple_interest", "is", interest)
