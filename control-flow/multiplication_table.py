1a2406ccc4609067baa4f0a2d725a7ca98db98ce
mnelisi90 <nmnelysn@gmail.com>
2024-12-22 15:14:20 +0200

    generate multiplication table for a user input number

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# define variable and prompt user to input number
+number = int(input("Enter a number to see its multiplication table:"))
+
+#generate loop
+for i in range(1,11): #loop from 1 to 11
+    result = number * i
+    print(number, "*", i, "=", result)
