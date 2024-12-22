17b1807fbe520256492432ee1103392b18406308
mnelisi90 <nmnelysn@gmail.com>
2024-12-22 19:16:51 +0200

    fix user prompt casing and variables

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,27 @@
 #define variable and prompt user to input task
-task = input("Enter task description:")
-time_bound = input("Is it time-bound? (yes/no):")
-priority = input("priority (high,medium,low:)")
+Task = input("Enter task description:")
+Time_Bound = input("Is it time-bound? (yes/no):")
+Priority = input("priority (high,medium,low:)")
 
 
 #match case stetament with priority
-match priority:
+match Priority:
     case "high":
-        print(f"Reminder: '{task}'is a high priority task", end="")
-        if time_bound == "yes":
+        print(f"Reminder: '{Task}'is a high priority task", end="")
+        if Time_Bound == "yes":
          print("that requires immediate attention today!")
         else:
          print("but can be done anytime today.")
 
     case "medium":
-        print(f"Reminder: '{task}'is a medium priority", end="")
-        if time_bound == "yes":
+        print(f"Reminder: '{Task}'is a medium priority", end="")
+        if Time_Bound == "yes":
          print(" that requires attention soon!")
         else:
          print(" but make sure you finish it by the end of the day.")
     case "low":
-        print(f"Reminder: '{task}' is a low priority", end="")
-        if time_bound == "yes":
+        print(f"Reminder: '{Task}' is a low priority", end="")
+        if Time_Bound == "yes":
          print("but it is time-sensitive, so try to complete it soon!")
         else:
          print(" and can be completed whenever you have free time.")
