854f82ab430190e4e3aee038320468de33da0f43
Mnelisi Victor Ntshapha <nmnelysn@gmail.com>
2024-12-22 15:33:09 +0200

    Update multiplication_table.py
    Add f-string in line 7 for a more concise and readable way

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,4 +4,4 @@ number = int(input("Enter a number to see its multiplication table:"))
 #generate loop
 for i in range(1,11): #loop from 1 to 11
     result = number * i
-    print(number, "*", i, "=", result)
+   print(f"{number} * {i} = {result}")
